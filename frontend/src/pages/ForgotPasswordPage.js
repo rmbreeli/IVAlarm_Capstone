b6ff16a7a6f3cc06212894fa9ad1f93b49ce409a
KristenBabbitt <kbabbitt15@gmail.com>
2025-03-04 11:55:34 -0600

    Small change to back button text on ForgotPassword page

--- frontend/src/pages/ForgotPasswordPage.js
+++ frontend/src/pages/ForgotPasswordPage.js
@@ -44,7 +44,7 @@ function ForgotPasswordPage() {
         </button>
 
         <button onClick={() => navigate("/")} className="login-button signup-button">
-          Back to Login
+          Back
         </button>
       </div>
     </div>
